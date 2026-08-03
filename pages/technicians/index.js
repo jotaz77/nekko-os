ede0a478640189cff1d5d508759863829e249877
Almir Cavalcante da Silva Junior <almir2cavalcante@gmail.com>
2026-08-03 16:22:08 -0300

    Update index.js

--- pages/technicians/index.js
+++ pages/technicians/index.js
@@ -1,1 +1,26 @@
+document.addEventListener("DOMContentLoaded", async () => {
 
+    try {
+
+        const result = await Bootstrap.init();
+
+        if (result.status !== "READY") {
+
+            window.location.href = "../login/login.html";
+            return;
+
+        }
+
+        console.log("✅ Tela de Técnicos carregada.");
+
+    }
+
+    catch (error) {
+
+        console.error(error);
+
+        alert(error.message);
+
+    }
+
+});
