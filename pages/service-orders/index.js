c6d33c351dc5672e1836516cb3c8d07fc876eed1
Almir Cavalcante da Silva Junior <almir2cavalcante@gmail.com>
2026-08-07 15:49:20 -0300

    Update index.js

--- pages/service-orders/index.js
+++ pages/service-orders/index.js
@@ -340,8 +340,8 @@ function renderOrders(orders) {
                     <button
                         class="view-order
                                 w-24
-                                bg-green-800
-                                hover:bg-green-600
+                                bg-red-800
+                                hover:bg-red-600
                                 py-1.5
                                 text-sm
                                 rounded-xl
