005719c84d00733f7d1c430b7a9de018f8c0bb61
Almir Cavalcante da Silva Junior <almir2cavalcante@gmail.com>
2026-09-07 16:21:57 -0300

    Update index.js

--- pages/service-orders/index.js
+++ pages/service-orders/index.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
         await loadServiceOrders(
             result.context,
-            "Aberta"
+            "ativas"
         );
 
         // ---------------------------------
